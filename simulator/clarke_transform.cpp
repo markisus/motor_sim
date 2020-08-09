7b1993abf35aa319f520d437f07c5e075691ffa0
Mark Liu <mark@biro.ai>
2020-08-08 23:48:33 -0400

    Adding Clarke transform

---
+++ simulator/clarke_transform.cpp
@@ -0,0 +1,21 @@
+#include "clarke_transform.h"
+#include <cmath>
+
+Eigen::Matrix<Scalar, 3, 3> make_clarke_transform() {
+    const Scalar sqrt2 = std::sqrt(2.0);
+    const Scalar sqrt3 = std::sqrt(3.0);
+    Eigen::Matrix<Scalar, 3, 3> clarke_transform;
+    clarke_transform <<
+        // clang-format off
+	1,        -1.0/2,   -1.0/2,
+	0,       sqrt3/2, -sqrt3/2,
+	1/sqrt2, 1/sqrt2,  1/sqrt2
+        // clang-format on
+        ;
+    clarke_transform *= sqrt2 / sqrt3;
+    return clarke_transform;
+}
+
+const Eigen::Matrix<Scalar, 3, 3> kClarkeTransform3x3 = make_clarke_transform();
+const Eigen::Matrix<Scalar, 2, 3> kClarkeTransform2x3 =
+    make_clarke_transform().block<2, 3>(0, 0);
---
+++ simulator/clarke_transform.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "scalar.h"
+#include <Eigen/Dense>
+
+extern const Eigen::Matrix<Scalar, 3, 3> kClarkeTransform3x3;
+extern const Eigen::Matrix<Scalar, 2, 3> kClarkeTransform2x3;
